d3daa8c120907cc2ce35e5f8703ffdc0b6109fe4
@DanekBB777 <bekenev.daniilkpc582020@gmail.com>
2023-02-11 22:59:07 +0300

    Axuenno

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 alert("Съебись отсюда");
+const buttonOK = document.querySelector("#buttonOk");
+const buttonClose = document.querySelector("#buttonClose");
+buttonOK.addEventListener("click", ()=>{
+    alert("Ты что ахуел на меня тыкать?")
+})
+
+buttonClose.addEventListener("click", ()=>{
+    alert("Тебе ебало закрыть?")
+})
